eefb3e927453e65cf5f3d3f7806d89aa1e0e738e
Carlos Quiroga <cquiro@gmail.com>
2018-04-28 22:31:18 -0300

    Add comment in app/models/shout.rb

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -4,6 +4,7 @@ class Shout < ApplicationRecord
   validates :body, presence: true, length: { in: 1..144 }
   validates :user, presence: true
 
+  # using default_scope is frowned upon. Check google before using it.
   default_scope { order(created_at: :desc) }
 
   delegate :username, to: :user
