9a425dd1e5037c264ea7c5239766a6c9d60c1d8b
Carlos Quiroga <cquiro@gmail.com>
2018-05-09 17:08:07 -0300

    Write spec for user creates shout feature using capybara.

---
+++ spec/features/user_creates_shout_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+require "support/features/clearance_helpers"
+
+feature 'User creates shout' do
+  scenario 'successfully' do
+    sign_in
+    fill_in 'shout_body', with: 'Hey, there!'
+    click_on 'Shout!'
+    expect(page).to have_css 'div', text: "Hey, there!"
+  end
+end
